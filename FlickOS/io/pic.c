230071d3a69d6b5e006547d75d7bf38419240087
Matthew Flickner UbuntuBeast VM <mwflickner@gmail.com>
2015-03-16 18:41:30 -0700

    working on getting the PIC up

--- FlickOS/io/pic.c
+++ FlickOS/io/pic.c
@@ -1,4 +1,5 @@
 #include "io.h"
+#include "idt.h"
 
 #define PIC1_PORT_A 0x20
 #define PIC2_PORT_A 0xA0
@@ -10,13 +11,36 @@
 
 #define PIC_ACK     0x20
 
+void pic_init(){
+    //Initalization
+    /* ICW1 */
+    outb( 0x20, 0x11 ); /* Master port A */
+    outb( 0xA0, 0x11 ); /* Slave port A */
+
+    /* ICW2 */
+    outb( 0x21, 0x20 ); /* Master offset of 0x20 in the IDT */
+    outb( 0xA1, 0x28 ); /* Master offset of 0x28 in the IDT */
+
+    /* ICW3 */
+    outb( 0x21, 0x04 ); /* Slaves attached to IR line 2 */
+    outb( 0xA1, 0x02 ); /* This slave in IR line 2 of master */
+
+    /* ICW4 */
+    outb( 0x21, 0x05 ); /* Set as master */
+    outb( 0xA1, 0x01 ); /* Set as slave */
+
+    //Set the interrupt mask
+    /* Only listen to irqs 0, 1, and 2 */
+    outb( 0x21, 0xf8 ); /* master PIC */
+    outb( 0xA1, 0xff ); /* slave PIC */
+
+}
 /** pic_acknowledge:
  *  Acknowledges an interrupt from either PIC 1 or PIC 2.
  *
  *  @param num The number of the interrupt
  */
-void pic_acknowledge(unsigned integer interrupt)
-{
+void pic_acknowledge(unsigned int interrupt){
     if (interrupt < PIC1_START_INTERRUPT || interrupt > PIC2_END_INTERRUPT) {
       return;
     }
@@ -27,3 +51,4 @@ void pic_acknowledge(unsigned integer interrupt)
       outb(PIC2_PORT_A, PIC_ACK);
     }
 }
+
---
+++ FlickOS/io/pic.h
@@ -0,0 +1,8 @@
+#ifndef INCLUDE_IO_H
+#define INCLUDE_IO_H
+
+void pic_init();
+
+void pic_acknowledge(unsigned int interrupt);
+
+#endif /* INCLUDE_IO_H */
