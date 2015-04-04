d06b8f1db6e9c0c13850d77aba5301ebc1fdec66
mwflickner <mwflickner@gmail.com>
2015-04-03 23:27:09 -0700

    attempting to run program

--- FlickOS/kmain.c
+++ FlickOS/kmain.c
@@ -6,9 +6,11 @@
 #include "pic.h"
 #include "gdt_idt.h"
 #include "printf.h"
+#include "multiboot.h"
 
 
-void kmain (){
+
+void kmain(unsigned int ebx){
     gdt_init();
 
     init_printf((void*) 0,putc);
@@ -23,7 +25,25 @@ void kmain (){
     printf("Address of idt_init() = %x \n", idt_init);
     idt_init();
     pic_init();
-    asm volatile ("int $0x3");
-    asm volatile ("int $0x4");
-    asm volatile ("int $33");
+    //asm volatile ("int $0x3");
+    //asm volatile ("int $0x4");
+    //asm volatile ("int $33");
+
+    multiboot_info_t *mbinfo = (multiboot_info_t *) ebx;
+    printf("mbinfo flags: %x \n", mbinfo->flags);
+    if(mbinfo->mods_count != 1){
+        char error[] = "error with mods_count \n";
+        fb_write(error, sizeof(error)-1);
+        return;
+    }
+    unsigned int address_of_module = mbinfo->mods_addr;
+    printf("address_of_module is %x \n", address_of_module);
+    typedef void (*call_module_t)(void);
+    /* ... */
+
+    call_module_t start_program = (call_module_t) address_of_module;
+    printf("about to start program \n");
+    start_program();
+    printf("program ran \n");
+    /* we'll never get here, unless the module code returns */
 }
--- FlickOS/loader.s
+++ FlickOS/loader.s
@@ -1,8 +1,9 @@
     global loader                                   ; the entry symbol for ELF
 
     MAGIC_NUMBER equ 0x1BADB002                     ; define the magic number constant
-    FLAGS        equ 0x0                            ; multiboot flags
-    CHECKSUM     equ -MAGIC_NUMBER                  ; calculate the checksum
+    ALIGN_MODULES equ 0x00000001
+    ;FLAGS        equ 0x0                            ; multiboot flags
+    CHECKSUM     equ -(MAGIC_NUMBER + ALIGN_MODULES)                  ; calculate the checksum
                                                     ; (magic number + checksum + flags should equal 0)
 
     KERNEL_STACK_SIZE equ 4096                      ; size of stack in bytes
@@ -15,7 +16,8 @@
     section .text                                   ; start of the text (code) section
     align 4                                         ; the code must be 4 byte aligned
         dd MAGIC_NUMBER                             ; write the magic number to the machine code,
-        dd FLAGS                                    ; the flags,
+        dd ALIGN_MODULES
+        ;dd FLAGS                                    ; the flags,
         dd CHECKSUM                                 ; and the checksum
 
 
@@ -23,7 +25,8 @@
         mov eax, 0xCAFEBABE                         ; place the number 0xCAFEBABE in the register eax
         mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the
                                                     ; stack (end of memory area)    
-        
+        push ebx
+
         extern kmain
         call kmain 
 
