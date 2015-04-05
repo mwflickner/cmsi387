806f8775c59e03f5a7727303402af3b123a90eb8
mwflickner <mwflickner@gmail.com>
2015-04-04 20:50:21 -0700

    shift characters work better

--- FlickOS/keyboard.c
+++ FlickOS/keyboard.c
@@ -18,7 +18,7 @@ unsigned char read_scan_code(void){
 }
 
 
-static unsigned char kbdus[128] =
+static unsigned char kbdus1[128] =
 {
     0,  27, '1', '2', '3', '4', '5', '6', '7', '8', /* 9 */
   '9', '0', '-', '=', '\b', /* Backspace */
@@ -57,6 +57,46 @@ static unsigned char kbdus[128] =
     0,  /* F12 Key */
     0,  /* All other keys are undefined */
 }; 
+
+static unsigned char kbdus2[128] =
+{
+    0, 27, '!', '@', '#', '$', '%', '^', '&', '*', /* 9 */
+    '(', ')', '_', '+', '\b', /* Backspace */
+    0,         /* Shift-Tab is zero for now */
+    'Q', 'W', 'E', 'R',   /* 19 */
+    'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\n', /* Enter key */
+    0,          /* 29   - Control */
+    'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', /* 39 */
+    '\"', '~',   0,        /* Left shift */
+    '\\', 'Z', 'X', 'C', 'V', 'B', 'N',            /* 49 */
+    'M', '<', '>', '?',   0,              /* Right shift */
+    '*',
+    0,  /* Alt */
+    ' ',  /* Space bar */
+    0,  /* Caps lock */
+    0,  /* 59 - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,  /* < ... F10 */
+    0,  /* 69 - Num lock*/
+    0,  /* Scroll Lock */
+    0,  /* Home key */
+    0,  /* Up Arrow */
+    0,  /* Page Up */
+    '-',
+    0,  /* Left Arrow */
+    0,
+    0,  /* Right Arrow */
+    '+',
+    0,  /* 79 - End key*/
+    0,  /* Down Arrow */
+    0,  /* Page Down */
+    0,  /* Insert Key */
+    0,  /* Delete Key */
+    0,   0,   0,
+    0,  /* F11 Key */
+    0,  /* F12 Key */
+    0,  /* All other keys are undefined */
+};
 
 enum {CapsRelease = 0xBA, leftShift = 0x2A, rightShift = 0x36, leftShiftRelease = 0xAA, rightShiftRelease= 0xB6};
 static uint8_t CapLocks = 0;
@@ -100,7 +140,14 @@ uint8_t convertToAscii(const uint8_t code){
         return 0;
     }
 
-    uint8_t temp = kbdus[code & 0x7F];
+    uint8_t temp;
+    if(ShiftL || ShiftR){
+        temp = kbdus2[code & 0x7F];
+        return temp;
+    }
+    else {
+        temp = kbdus1[code & 0x7F];
+    }
 
     if(CapLocks && (ShiftL || ShiftR)){
         return temp;
